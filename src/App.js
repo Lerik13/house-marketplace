b953704efcb40523711ce790a4af5c083eed7a4c
Valeria <lerik13@gmail.com>
2022-03-30 20:25:45 -0600

    added Pages, Routes, Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Explore from "./pages/Explore";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
 	return (
 		<>
-			<h1>App</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Explore />} />
+					<Route path="/offers" element={<Offers />} />
+					<Route path="/profile" element={<SignIn />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+				</Routes>
+				<Navbar />
+			</BrowserRouter>
 		</>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+/*
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
+*/
 //reportWebVitals();
