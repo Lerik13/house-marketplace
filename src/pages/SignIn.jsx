dab6e4f75d46f85cefdd686a9dd1fc2b369fe136
Valeria <lerik13@gmail.com>
2022-03-30 23:33:46 -0600

    SignIn, SignUp pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,83 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn() {
-  return (
-	<div>SignIn</div>
-  )
+	const [showPassword, setShowPassword] = useState(false);
+	const [formData, setFormData] = useState({
+		email: '',
+		password: ''
+	});
+	const {email, password} = formData
+
+	const navigate = useNavigate()
+	
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}))
+	}
+
+	return (
+		<>
+			<div className="pageContainer">
+				<header>
+					<p className="pageHeader">Welcome Back!</p>
+				</header>
+
+				<main>
+					<form>
+						<input
+							type="email"
+							className="emailInput"
+							placeholder='Email'
+							id="email"
+							value={email}
+							onChange={onChange}
+						/>
+
+						<div className="passwordInputDiv">
+							<input 
+								type={showPassword ? 'text' : 'password'}
+								className='passwordInput'
+								placeholder='Password'
+								id='password'
+								value={password}
+								onChange={onChange}
+							/>
+							<img 
+								src={visibilityIcon} 
+								alt="Show password" 
+								className="showPassword"
+								onClick={() => setShowPassword(prev => !prev)}
+							/>
+						</div>
+
+						<Link to='/forgot-password' className='forgotPasswordLink'>
+							Forgot Password
+						</Link>
+
+						<div className="signInBar">
+							<p className="signInText">
+								Sign In
+							</p>
+							<button className="signInButton">
+								<ArrowRightIcon fill='#fff' width='34px' height='34px' />
+							</button>
+						</div>
+					</form>
+
+					{/* Google OAuth */}
+
+					<Link to='/sign-up' className='registerLink'>
+						Sign Up Instead
+					</Link>
+				</main>
+			</div>
+		</>
+	)
 }
 
 export default SignIn
