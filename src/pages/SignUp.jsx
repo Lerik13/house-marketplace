9199f75d24eea407a11ca682ed4c1bfc2c8246ff
Valeria <lerik13@gmail.com>
2022-04-02 09:50:09 -0600

    sign-in, sign-up with Google OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -6,6 +6,7 @@ import { setDoc, doc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+import OAuth from '../components/OAuth';
 
 function SignUp() {
 	const [showPassword, setShowPassword] = useState(false);
@@ -108,7 +109,7 @@ function SignUp() {
 						</div>
 					</form>
 
-					{/* Google OAuth */}
+					<OAuth />
 
 					<Link to='/sign-in' className='registerLink'>
 						Sign In Instead
