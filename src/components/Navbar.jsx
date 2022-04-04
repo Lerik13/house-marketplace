a8790a56391c061a62f33ba9a0d58d1c8594dc3f
Valeria <lerik13@gmail.com>
2022-04-04 16:33:20 -0600

    Offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
 					</li>
 					<li className="navbarListItem" onClick={() => navigate('/offers')}>
 						<OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2' : '#8f8f8f'} width='36px' height='36px' />
-						<p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offer</p>
+						<p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offers</p>
 					</li>
 					<li className="navbarListItem" onClick={() => navigate('/profile')}>
 						<PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2' : '#8f8f8f'} width='36px' height='36px' />
