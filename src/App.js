25a2998750746d3a637fb6ce461baee1403f2702
Valeria <lerik13@gmail.com>
2022-04-06 15:02:43 -0600

    Listing page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Explore from "./pages/Explore";
 import ForgotPassword from "./pages/ForgotPassword";
+import Listing from "./pages/Listing";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
@@ -20,6 +21,7 @@ function App() {
 					<Route path="/" element={<Explore />} />
 					<Route path="/offers" element={<Offers />} />
 					<Route path="/category/:categoryName" element={<Category />} />
+					<Route path="/category/:categoryName/:listingId" element={<Listing />} />
 					<Route path="/profile" element={<PrivateRoute />}>
 						<Route path="/profile" element={<Profile />} />
 					</Route>
